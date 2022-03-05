1c43a046459ae45e0ce2c5d9b1f32e5afa426f72
rukiyesenturk <rukiyeyagmur28@gmail.com>
2022-03-05 20:16:19 +0300

    Initial Commit

---
+++ NewsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsProject
+//
+//  Created by Macbook on 5.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
